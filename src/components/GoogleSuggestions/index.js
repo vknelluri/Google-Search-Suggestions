75f822fcc59ee617516cfefaa36f8031c00d559d
root <vknelluri@gmail.com>
2024-05-24 10:35:59 +0000

    Solution

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,65 @@
+// Write your code here
+import React, {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  updateSearchInput = suggestion => {
+    this.setState({searchInput: suggestion})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {suggestionsList} = this.props
+    const searchResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="google-suggestions-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+          alt="google logo"
+          className="google-logo"
+        />
+        <div className="search-container">
+          <div className="search-input-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              alt="search icon"
+              className="search-icon"
+            />
+            <input
+              type="search"
+              className="search-input"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+              placeholder="Search Google"
+            />
+          </div>
+          <ul className="suggestions-list">
+            {searchResults.map(eachSuggestion => (
+              <SuggestionItem
+                key={eachSuggestion.id}
+                suggestionDetails={eachSuggestion}
+                updateSearchInput={this.updateSearchInput}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
